0967be32537296e19f365584944e00c71f6fe273
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-10-19 00:44:25 +0900

    Bump hashicorp/azurerm

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.63.0"
+      version = "~>3.90.0"
     }
     random = {
       source  = "hashicorp/random"
