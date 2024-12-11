60daf7f26da0429a8e10fa7ec0b462c1634fab42
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2024-12-11 22:44:42 +0900

    Bump sendgrid

--- modules/sendgrid/main.tf
+++ modules/sendgrid/main.tf
@@ -1,12 +1,29 @@
 resource "sendgrid_api_key" "web" {
   name = var.fqdn
   scopes = [
-    "2fa_exempt",
-    "2fa_required",
+    # "2fa_exempt",
+    # "2fa_required",
     "mail.send",
-    "sender_verification_exempt"
+    # "sender_verification_exempt"
   ]
+
+  lifecycle {
+    ignore_changes = [
+      scopes,
+    ]
+  }
 }
+
+# resource "sendgrid_teammate" "user" {
+#   email  = "user@example.com"
+#   scopes = var.teammate_scopes
+
+#   lifecycle {
+#     ignore_changes = [
+#       scopes,
+#     ]
+#   }
+# }
 
 resource "sendgrid_sender_authentication" "fqdn" {
   domain = var.fqdn
@@ -14,4 +31,8 @@ resource "sendgrid_sender_authentication" "fqdn" {
 
 resource "sendgrid_link_branding" "fqdn" {
   domain = var.fqdn
+}
+
+resource "sendgrid_click_tracking_settings" "web" {
+  enabled = false
 }
